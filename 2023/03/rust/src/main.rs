532933b9aa13954a496dcc4d728bfff342d93166
Andrea Gilmozzi <agilmozzi@gmail.com>
2023-12-22 19:42:25 +0100

    03 done

--- 2023/03/rust/src/main.rs
+++ 2023/03/rust/src/main.rs
@@ -45,12 +45,48 @@ fn get_close_numbers(source: &Vec<char>, width: usize, x: usize, y: usize) -> Ve
     r
 }
 
+fn clean_number(source: &mut Vec<char>, start: usize, number: u32) {
+    let s_number = number.to_string();
+
+    for x in 0..s_number.len() {
+        source[start + x] = '.';
+    }
+}
+
+fn fetch_number(source: &mut Vec<char>, width: usize, x: usize, y: usize) -> u32 {
+    let mut start = y * (width + 1) + x;
+
+    while let Some(x) = source.get(start) {
+        if start > 0 && x.is_numeric() {
+            start -= 1;
+        } else {
+            break;
+        }
+    }
+
+    if start > 0 {
+        start += 1;
+    }
+
+    let t_source = &source[start..];
+    let number = t_source
+        .iter()
+        .map_while(|c| c.to_digit(10))
+        .fold(0, |acc, d| acc * 10 + d);
+
+    clean_number(source, start, number);
+
+    number
+}
+
 fn main() {
     let mut schematics = Vec::<char>::new();
     let mut width: usize = 0;
     let mut height: usize = 0;
     let mut symbols = Vec::<(usize, usize)>::new();
-    let mut starting_number = Vec::<(usize, usize)>::new();
+    // let mut starting_number = Vec::<(usize, usize)>::new();
+    let mut total: u32 = 0;
+    let mut gears_ratio: u32 = 0;
 
     for line in io::stdin().lock().lines() {
         let mut x: usize = 0;
@@ -65,17 +101,34 @@ fn main() {
         width = x
     }
 
-    height -= 1;
+    // height -= 1;
     width -= 1;
 
-    println!("{:?}", schematics);
-    println!("symbols {:?}", symbols);
-    println!("sizes {:?}", (width, height));
+    // println!("{:?}", schematics);
+    // println!("symbols {:?}", symbols);
+    // println!("sizes {:?}", (width, height));
 
     for (x, y) in symbols {
-        starting_number.extend(get_close_numbers(&schematics, width, x, y));
+        let mut c_numbers = Vec::<u32>::new();
+        for (x, y) in get_close_numbers(&schematics, width, x, y) {
+            let n = fetch_number(&mut schematics, width, x, y);
+            c_numbers.push(n);
+            total += n;
+        }
+
+        c_numbers.retain(|&x| x != 0);
+        if let Some(s) = get_value(&schematics, width, x, y) {
+            if *s == '*' && c_numbers.len() == 2 {
+                gears_ratio += c_numbers.iter().fold(1, |acc, n| acc * n);
+            }
+        }
     }
 
-    println!("numbers {:?}", starting_number);
-    println!("test {:?}", get_close_numbers(&schematics, width, 1, 0));
+    // println!("numbers {:?}", starting_number);
+
+    // println!("{:?}", fetch_number(&mut schematics, width, 2, 2));
+    // println!("{:?}", schematics);
+
+    println!("{}", total);
+    println!("{}", gears_ratio);
 }
