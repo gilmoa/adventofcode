3726ffa42fc9fe863de47d370dbaf7542cda5775
Andrea Gilmozzi <agilmozzi@gmail.com>
2023-12-26 22:57:58 +0100

    end day 08

--- 2023/08/rust/src/main.rs
+++ 2023/08/rust/src/main.rs
@@ -10,6 +10,22 @@ struct Node {
     right: String,
 }
 
+fn gdc(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gdc(b, a % b)
+}
+
+fn lcm(n: &[usize]) -> usize {
+    if n.len() == 1 {
+        return n[0];
+    }
+    let a = n[0];
+    let b = lcm(&n[1..]);
+    a * b / gdc(a, b)
+}
+
 fn main() {
     let mut lines = io::stdin().lock().lines();
     let instructions: Vec<char> = lines
@@ -42,23 +58,24 @@ fn main() {
         nodes.insert(id.clone(), Node { id, left, right });
     }
 
-    let mut steps: u32 = 0;
+    let mut steps: usize = 0;
     let mut c_nodes: Vec<&Node> = nodes
         .iter()
         .filter(|(k, _)| k.ends_with('A'))
         .map(|(_, v)| v)
         .collect();
 
-    println!("c_nodes: {:?}", c_nodes);
+    let mut c_steps = vec![0usize; c_nodes.len()];
+    // println!("c_nodes: {:?}", c_nodes);
 
-    while steps < u32::MAX {
-        let index = steps as usize % instructions.len();
+    while steps < usize::MAX {
+        let index = steps % instructions.len();
         let dir = instructions
             .get(index)
             .expect("Should loop back and always have a valid index");
 
-        let mut should_stop = true;
-        for c_node in c_nodes.iter_mut() {
+        steps += 1;
+        for (i, c_node) in c_nodes.iter_mut().enumerate() {
             let n_node = if *dir == 'L' {
                 &c_node.left
             } else {
@@ -67,22 +84,22 @@ fn main() {
 
             *c_node = nodes.get(n_node).expect("Nodes should have valid nodes.");
 
-            if !c_node.id.ends_with('Z') {
-                should_stop = false
+            if c_node.id.ends_with('Z') && c_steps[i] == 0 {
+                c_steps[i] = steps;
+                // println!("{:?}", c_steps);
             }
-            // println!("{steps} : {dir} - {:?}", c_node.id);
         }
 
-        steps += 1;
-
-        if should_stop {
+        if c_steps
+            .iter()
+            .map(|v| if *v > 0 { 1 } else { 0 })
+            .product::<usize>()
+            != 0
+        {
             break;
-        }
-
-        if steps % 1000000 == 0 {
-            println!("{steps} / {}", usize::MAX);
         }
     }
 
-    println!("{steps}");
+    println!("{:?}", lcm(&c_steps));
+    // println!("{steps}");
 }
