f04b212e07e79af848666724e6fd48e176308a0c
Andrea Gilmozzi <agilmozzi@gmail.com>
2024-01-06 02:40:02 +0100

    solve day 11 part 2

--- 2023/11/rust/src/main.rs
+++ 2023/11/rust/src/main.rs
@@ -1,22 +1,26 @@
 use std::{
-    cmp::min,
+    cmp::{max, min},
     fmt::{Display, Formatter, Result},
     io,
 };
 
-struct Map {
+struct Map<'a> {
     width: usize,
     height: usize,
-    grid: Vec<char>,
+    grid: &'a mut Vec<char>,
+    exp_rows: Vec<usize>,
+    exp_cols: Vec<usize>,
 }
 
-impl Map {
-    fn new(width: usize, grid: Vec<char>) -> Self {
+impl<'a> Map<'a> {
+    fn new(width: usize, grid: &'a mut Vec<char>) -> Self {
         let height = grid.len() / width;
         Self {
             width,
             height,
             grid,
+            exp_rows: Vec::new(),
+            exp_cols: Vec::new(),
         }
     }
 
@@ -27,6 +31,36 @@ impl Map {
     // fn get(&self, x: usize, y: usize) -> Option<&char> {
     //     self.grid.get(y * self.width + x)
     // }
+
+    fn calc_expand(&mut self) {
+        let empty_row = ".".repeat(self.width);
+        for y in 0..self.height {
+            let start = y * self.width;
+            let slice: String = self
+                .grid
+                .get(start..start + self.width)
+                .expect("Should always find complete line")
+                .iter()
+                .collect();
+            if slice == empty_row {
+                self.exp_rows.push(y);
+            }
+        }
+
+        let empty_col = ".".repeat(self.height);
+        for x in 0..self.width {
+            let slice: String = self
+                .grid
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| i % self.width == x)
+                .map(|(_, ch)| ch)
+                .collect();
+            if slice == empty_col {
+                self.exp_cols.push(x)
+            }
+        }
+    }
 
     fn expand(&mut self) {
         // expand rows
@@ -73,7 +107,7 @@ impl Map {
     }
 }
 
-impl Display for Map {
+impl<'a> Display for Map<'a> {
     fn fmt(&self, f: &mut Formatter) -> Result {
         let mut r: String = "".to_string();
 
@@ -84,13 +118,46 @@ impl Display for Map {
             r.push(ch);
         }
 
-        write!(f, "{}\nw{}h{}", r.trim(), self.width, self.height)
+        write!(
+            f,
+            "{}\nw{}h{}\nexp_rows: {:?}\nexp_cols: {:?}",
+            r.trim(),
+            self.width,
+            self.height,
+            self.exp_rows,
+            self.exp_cols
+        )
     }
 }
 
-fn get_distance(a: (usize, usize), b: (usize, usize)) -> usize {
-    let d_x = a.0.abs_diff(b.0);
-    let d_y = a.1.abs_diff(b.1);
+fn get_distance(
+    a: (usize, usize),
+    b: (usize, usize),
+    exp_x: &Vec<usize>,
+    exp_y: &Vec<usize>,
+    exp_factor: usize,
+) -> usize {
+    let x_min = min(a.0, b.0);
+    let x_max = max(a.0, b.0);
+
+    let y_min = min(a.1, b.1);
+    let y_max = max(a.1, b.1);
+
+    let mut d_x = x_max - x_min;
+    let mut d_y = y_max - y_min;
+
+    for x in exp_x {
+        if (x_min..x_max).contains(x) {
+            d_x += exp_factor - 1;
+        }
+    }
+
+    for y in exp_y {
+        if (y_min..y_max).contains(y) {
+            d_y += exp_factor - 1;
+        }
+    }
+
     d_x + d_y
 }
 
@@ -105,10 +172,9 @@ fn main() {
         }
     }
 
-    let mut map = Map::new(width, grid);
+    let mut map = Map::new(width, &mut grid);
 
-    // println!("{map}");
-    map.expand();
+    map.calc_expand();
     // println!("{map}");
 
     // find galaxies
@@ -123,17 +189,30 @@ fn main() {
     // println!("Galaxies: {galaxies:?}");
 
     // let mut galaxies_pairs: Vec<(usize, usize)> = Vec::new();
-    let mut galaxies_distances: Vec<usize> = Vec::new();
+    // let mut galaxies_distances: Vec<usize> = Vec::new();
+    let mut t_distance2: usize = 0;
+    let mut t_distance10: usize = 0;
+    let mut t_distance100: usize = 0;
+    let mut t_distance1000000: usize = 0;
+
     for a in 0..galaxies.len() - 1 {
         for b in a + 1..galaxies.len() {
             // galaxies_pairs.push((a, b));
             let g_a = galaxies[a];
             let g_b = galaxies[b];
-            galaxies_distances.push(get_distance(g_a, g_b));
+            // galaxies_distances.push(get_distance(g_a, g_b));
+            t_distance2 += get_distance(g_a, g_b, &map.exp_cols, &map.exp_rows, 2);
+            t_distance10 += get_distance(g_a, g_b, &map.exp_cols, &map.exp_rows, 10);
+            t_distance100 += get_distance(g_a, g_b, &map.exp_cols, &map.exp_rows, 100);
+            t_distance1000000 += get_distance(g_a, g_b, &map.exp_cols, &map.exp_rows, 1000000);
         }
     }
 
     // println!("{}", galaxies_pairs.len());
     // println!("Distances : {galaxies_distances:?}");
-    println!("{}", galaxies_distances.iter().sum::<usize>());
+    // println!("{}", galaxies_distances.iter().sum::<usize>());
+    println!("{t_distance2}");
+    println!("{t_distance10}");
+    println!("{t_distance100}");
+    println!("{t_distance1000000}");
 }
