289bdcc3b315a24fff0e92f7f9ea5040d9e441aa
Andrea Gilmozzi <agilmozzi@gmail.com>
2024-01-07 00:03:13 +0100

    use u32

--- 2023/rust/src/bin/02.rs
+++ 2023/rust/src/bin/02.rs
@@ -1,25 +1,24 @@
 use std::{cmp::max, collections::HashMap, io::BufRead};
 
 fn main() {
-    let fixed_bag: HashMap<&str, i32> = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
+    let fixed_bag: HashMap<&str, u32> = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
 
     let mut total_games = 0;
     let mut total_power = 0;
     for line in std::io::stdin().lock().lines() {
         let cline = line.unwrap_or(String::from(""));
         let parts: Vec<&str> = cline.split(":").collect();
-        assert_eq!(parts.len(), 2);
         let gameid = parts[0].replace("Game ", "").parse::<i32>().unwrap();
 
         let mut valid_game = true;
 
-        let mut min_bag: HashMap<String, i32> = HashMap::new();
+        let mut min_bag: HashMap<String, u32> = HashMap::new();
 
         for set in parts[1].split(";") {
             for cset in set.trim().split(",") {
                 let parts: Vec<&str> = cset.trim().split_whitespace().collect();
                 let gc: String = parts[1].trim().to_lowercase();
-                let gn: i32 = parts[0].parse::<i32>().unwrap();
+                let gn: u32 = parts[0].parse::<u32>().unwrap();
                 if fixed_bag.contains_key(gc.as_str()) {
                     if gn > fixed_bag[gc.as_str()] {
                         valid_game = false;
