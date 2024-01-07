e52c674ade824e7ca90c388083f00b82c47e6d71
Andrea Gilmozzi <agilmozzi@gmail.com>
2024-01-07 12:37:38 +0100

    solve day 12 part 2

--- 2023/rust/src/bin/12.rs
+++ 2023/rust/src/bin/12.rs
@@ -1,22 +1,20 @@
 use std::{
+    collections::HashMap,
     io::{self, BufRead},
-    time::{SystemTime, UNIX_EPOCH},
+    iter,
 };
 
-fn reduce_line(l: &String) -> String {
-    let mut r = "".to_string();
-    let mut p_ch = '.';
-    for c in l.chars() {
-        if p_ch != '.' || c != '.' {
-            r.push(c);
-        }
-        p_ch = c
-    }
-
-    r
+fn get_state_hash(s: &str, g: &Vec<usize>) -> String {
+    format!("{s}-{:?}", g)
 }
 
-fn count_valid_permutations(s: String, g: Vec<usize>) -> usize {
+fn count_valid_permutations(s: String, g: Vec<usize>, c: &mut HashMap<String, usize>) -> usize {
+    let hash = get_state_hash(s.as_str(), &g);
+
+    if let Some(x) = c.get(&hash) {
+        return *x;
+    }
+
     if s.len() < 1 {
         return match g.len() < 1 {
             true => 1,
@@ -55,56 +53,63 @@ fn count_valid_permutations(s: String, g: Vec<usize>) -> usize {
     }
 
     let c_ch = &s[..1];
-
-    if c_ch == "." {
-        return count_valid_permutations(s[1..].to_string(), g.clone());
-    }
+    let mut r: usize = 0;
 
     if c_ch == "?" {
-        let a = count_valid_permutations(".".to_string() + &s[1..], g.clone());
-        let b = count_valid_permutations("#".to_string() + &s[1..], g.clone());
-        return a + b;
-    }
-
-    if c_ch == "#" {
+        let a = count_valid_permutations(".".to_string() + &s[1..], g.clone(), c);
+        let b = count_valid_permutations("#".to_string() + &s[1..], g.clone(), c);
+        r = a + b;
+    } else if c_ch == "#" {
         if is_group && &s[c_g..c_g + 1] != "#" {
-            return count_valid_permutations(s[c_g + 1..].to_string(), g[1..].to_vec());
-        } else {
-            return 0;
+            r = count_valid_permutations(s[c_g + 1..].to_string(), g[1..].to_vec(), c);
         }
+    } else {
+        // c_ch == "."
+        r = count_valid_permutations(s[1..].to_string(), g.clone(), c);
     }
 
-    count_valid_permutations(s[1..].to_string(), g.clone())
+    c.insert(hash, r);
+    return r;
 }
 
 fn main() {
-    let mut t_permutations: usize = 0;
+    let mut t_permutations1: usize = 0;
+    let mut t_permutations2: usize = 0;
 
-    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let mut cache: HashMap<String, usize> = HashMap::new();
 
-    for (i, line) in io::stdin().lock().lines().enumerate() {
+    for (_i, line) in io::stdin().lock().lines().enumerate() {
         let line = line.expect("Should always parse a line");
         let mut s_line = line.split_whitespace();
-        let springs = s_line
+        let springs1 = s_line
             .next()
             .expect("Line should always split in two by whitespace")
-            .to_string()
-            + ".";
+            .to_string();
 
-        let groups: Vec<usize> = s_line
+        let groups1: Vec<usize> = s_line
             .next()
             .expect("Line should always split in two by whitespace")
             .split(',')
             .map(|n| n.parse::<usize>().unwrap())
             .collect();
 
-        let c_permutations = count_valid_permutations(springs, groups);
-        println!("{i} = {c_permutations}");
-        t_permutations += c_permutations;
+        let springs2 = iter::repeat(springs1.clone())
+            .take(5)
+            .map(String::from)
+            .collect::<Vec<String>>()
+            .join("?");
+
+        let groups2 = groups1.repeat(5);
+
+        let c_permutations1 = count_valid_permutations(springs1, groups1, &mut cache);
+        t_permutations1 += c_permutations1;
+
+        let c_permutations2 = count_valid_permutations(springs2, groups2, &mut cache);
+        t_permutations2 += c_permutations2;
+
+        // println!("{_i} = {c_permutations1}, {c_permutations2}");
     }
 
-    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    dbg!(end - start);
-
-    println!("{t_permutations}");
+    println!("{t_permutations1}");
+    println!("{t_permutations2}");
 }
