4e93de1fffb64877e05c1f91a9fd32c3eda2a86a
Andrea Gilmozzi <agilmozzi@gmail.com>
2024-01-08 21:38:12 +0100

    day13 testing

--- 2023/rust/src/bin/13.rs
+++ 2023/rust/src/bin/13.rs
@@ -1,59 +1,75 @@
-use std::{
-    cmp::min,
-    io::{self, BufRead},
-};
+use std::io::{self, BufRead};
 
-struct Map {
-    grid: Vec<char>,
-    width: usize,
-    height: usize,
+fn is_reflected(s: &str) -> bool {
+    let s1: Vec<char> = s.chars().collect();
+    let s2: Vec<char> = s1.iter().cloned().rev().collect();
+
+    println!("{s} ref: {}", s1 == s2);
+
+    s1 == s2
 }
 
-impl Map {
-    fn to_string(&self) -> String {
-        let mut r = "".to_string();
-
-        for (i, ch) in self.grid.iter().enumerate() {
-            if i % self.width == 0 {
-                r.push('\n');
-            }
-            r.push(*ch)
-        }
-
-        return r.trim().to_string();
+fn get_reflection(s: &str) -> usize {
+    let h_len = s.len() / 2;
+    if h_len < 1 {
+        return 0;
     }
+    for x in 1..s.len() - 1 {
+        println!("{s} reflecting at {x}");
+        let r_len = match h_len < x {
+            true => s.len() - x,
+            false => x,
+        };
+        let start = x - r_len;
+        let end = x + r_len;
+        let s1 = &s[start..end];
+        println!("reflected {s1} with {r_len}");
+
+        if is_reflected(s1) {
+            return x;
+        }
+    }
+
+    0
 }
 
 fn main() {
-    let mut buffer = "".to_string();
+    let mut row = "".to_string();
+    let mut col = "".to_string();
+
+    let mut total1: usize = 0;
+
     for line in io::stdin().lock().lines() {
-        let mut line = line.expect("Should always get a line");
+        let line = line.expect("Should always get a line");
         if !line.is_empty() {
-            line.push('\n');
-            buffer.push_str(&line);
+            row = line.clone();
+            col.push_str(&line[..1]);
         } else {
-            buffer = buffer.trim().to_string();
-            // println!("{buffer}");
-            if !buffer.is_empty() {
-                let mut grid: Vec<char> = Vec::new();
-                let mut width: usize = usize::MAX;
-                for line in buffer.lines() {
-                    width = min(width, line.len());
-                    for ch in line.chars() {
-                        grid.push(ch);
-                    }
-                }
+            // check row
 
-                let height = grid.len() / width;
-                let map: Map = Map {
-                    grid,
-                    width,
-                    height,
-                };
+            let ref_r = get_reflection(&row);
+            let ref_c = match ref_r > 0 {
+                true => 0,
+                false => get_reflection(&col),
+            };
+            let total = (ref_r) + 100 * (ref_c);
 
-                println!("{}", map.to_string());
-            }
-            buffer.clear();
+            total1 += total;
+            println!("reflection at {ref_r}");
+            println!("reflection at {ref_c}");
+            println!("total at {total}");
+            col.clear();
         }
     }
+
+    let ref_r = get_reflection(&row);
+    let ref_c = get_reflection(&col);
+    let total = (ref_r) + 100 * (ref_c);
+
+    total1 += total;
+
+    println!("reflection at {ref_r}");
+    println!("reflection at {ref_c}");
+
+    println!("{total1}");
 }
