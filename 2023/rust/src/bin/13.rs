e414897237bd0c34e23ac0caf85a23f8ad024990
Andrea Gilmozzi <agilmozzi@gmail.com>
2024-01-09 00:48:24 +0100

    day 13 part 1 solved

--- 2023/rust/src/bin/13.rs
+++ 2023/rust/src/bin/13.rs
@@ -1,75 +1,91 @@
+use core::fmt::Debug;
 use std::io::{self, BufRead};
 
-fn is_reflected(s: &str) -> bool {
-    let s1: Vec<char> = s.chars().collect();
-    let s2: Vec<char> = s1.iter().cloned().rev().collect();
+fn is_reflected<T: Clone + Debug + PartialEq>(s: Vec<T>) -> bool {
+    let s1: Vec<T> = s.iter().cloned().rev().collect();
 
-    println!("{s} ref: {}", s1 == s2);
+    // println!("{:?} ref: {}", s, s == s1);
 
-    s1 == s2
+    s == s1
 }
 
-fn get_reflection(s: &str) -> usize {
-    let h_len = s.len() / 2;
-    if h_len < 1 {
+fn get_reflection(map: &Vec<String>) -> usize {
+    if map.len() < 2 {
         return 0;
     }
-    for x in 1..s.len() - 1 {
-        println!("{s} reflecting at {x}");
-        let r_len = match h_len < x {
-            true => s.len() - x,
-            false => x,
-        };
-        let start = x - r_len;
-        let end = x + r_len;
-        let s1 = &s[start..end];
-        println!("reflected {s1} with {r_len}");
 
-        if is_reflected(s1) {
-            return x;
+    let h_len = map[0].len() / 2;
+
+    let mut r: usize = 0;
+    for x in 1..map[0].len() {
+        // println!("+++++++ checking reflection at {x}");
+
+        let mut x_valid = true;
+        for s in map {
+            let r_len = match h_len < x {
+                true => s.len() - x,
+                false => x,
+            };
+            let start = x - r_len;
+            let end = x + r_len;
+            let s1 = &s[start..end];
+
+            // println!("checking {s1} with {r_len}");
+            if !is_reflected(s1.into()) {
+                x_valid = false;
+                break;
+            }
+        }
+        if x_valid {
+            // println!("--- reflection at {x}");
+            r += x;
         }
     }
 
-    0
+    r
 }
 
 fn main() {
-    let mut row = "".to_string();
-    let mut col = "".to_string();
+    let mut rows: Vec<String> = Vec::new();
 
     let mut total1: usize = 0;
+
+    let mut counter: usize = 1;
 
     for line in io::stdin().lock().lines() {
         let line = line.expect("Should always get a line");
         if !line.is_empty() {
-            row = line.clone();
-            col.push_str(&line[..1]);
+            rows.push(line.clone())
         } else {
-            // check row
+            let mut cols: Vec<String> = Vec::new();
+            for s in &rows {
+                for (x, ch) in s.char_indices() {
+                    if let Some(t) = cols.get_mut(x) {
+                        t.push(ch);
+                    } else {
+                        cols.insert(x, ch.to_string())
+                    }
+                }
+            }
 
-            let ref_r = get_reflection(&row);
-            let ref_c = match ref_r > 0 {
-                true => 0,
-                false => get_reflection(&col),
-            };
+            println!("======{counter:03}======");
+            for x in &rows {
+                println!("{counter:03} {x}");
+            }
+            let ref_r = get_reflection(&rows);
+            println!("{counter:03} reflection at ROW {ref_r}");
+
+            let ref_c = get_reflection(&cols);
+            println!("{counter:03} reflection at COL {ref_c}");
+
             let total = (ref_r) + 100 * (ref_c);
 
             total1 += total;
-            println!("reflection at {ref_r}");
-            println!("reflection at {ref_c}");
-            println!("total at {total}");
-            col.clear();
+            println!("=={counter:03} total {total}=");
+            rows.clear();
+            counter += 1;
         }
     }
-
-    let ref_r = get_reflection(&row);
-    let ref_c = get_reflection(&col);
-    let total = (ref_r) + 100 * (ref_c);
-
-    total1 += total;
-
-    println!("reflection at {ref_r}");
-    println!("reflection at {ref_c}");
 
     println!("{total1}");
 }
